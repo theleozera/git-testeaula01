43b72247c60685466517df92bdb460a2f30e0451
leo <lpdoliveira@restinga.ifrs.edu.br>
2021-05-05 19:32:50 -0300

    for inserido no programa

--- src/aplication/programa.java
+++ src/aplication/programa.java
@@ -6,7 +6,12 @@ public class programa {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
 		int a = 9;
+		int b= 0;
 		System.out.println("valor de a=" + a);
-	}
-
+		for( b = 0; b<10; b++) {
+			System.out.println("ainda não estamos em 10 estamos em " + b);
+		}
+	
+	System.out.println("é 10!!!");
+}
 }
